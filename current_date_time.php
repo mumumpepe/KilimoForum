fc7686d9441210faddc17149df897923840d6720
mumumpepe <abinerykinwiko@yahoo.com>
2024-09-02 11:43:13 +0300

    date and time

---
+++ current_date_time.php
@@ -0,0 +1,11 @@
+<?php
+// Set the default timezone to your preferred timezone
+date_default_timezone_set('America/New_York');
+
+// Get the current date and time
+$currentDateTime = date('Y-m-d H:i:s');
+
+// Display the current date and time
+echo "<h1>Current Date and Time</h1>";
+echo "<p>The current date and time is: $currentDateTime</p>";
+?>
