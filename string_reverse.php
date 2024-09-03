98b1ec06539e988178625401c73b8d5f5c54d410
mumumpepe <abinerykinwiko@yahoo.com>
2024-09-03 09:08:28 +0300

    string_reverse

---
+++ string_reverse.php
@@ -0,0 +1,11 @@
+<?php
+// Define a string
+$originalString = "Hello, World!";
+
+// Reverse the string
+$reversedString = strrev($originalString);
+
+// Display the original and reversed strings
+echo "Original string: $originalString<br>";
+echo "Reversed string: $reversedString";
+?>
